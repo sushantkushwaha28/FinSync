ce6f6de660a82bc2c0db12c684a189efb90844d4
Sushant <sushantkushwaha289@gmail.com>
2025-02-26 15:18:16 +0530

    database

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 import HeroSection from "@/components/hero";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { featuresData, howItWorksData, statsData } from "@/data/landing";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -56,8 +58,14 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="py-209">
-
+      <section className="py-20 bg-blue-600">
+        <div className="container mx-auto px-4 text-center"></div>
+        <h2 className="text-3xl font-bold text-center text-white mb-6">Ready to Take Control of  Your Finances ?</h2>
+          <p className="text-blue-100 mb-8 max-w-2xl mx-auto">Join thousands of users who are already managing their finances smarter with FinSync</p>
+          <Link href="/dashboard" className="grid place-items-center">
+          <Button size="lg"
+           className="bg-white text-blue-600 hover:bg-black animate-bounce">Start Free Trial</Button>
+           </Link>
       </section>
     </div>
     );
